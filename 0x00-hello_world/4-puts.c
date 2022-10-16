667a86b687f801c14ed025de435a0cbdc628801b
gift <treasureluke23@gmail.com>
2022-10-16 00:13:02 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ * main - print line of code using puts
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	Return (0);
+}
