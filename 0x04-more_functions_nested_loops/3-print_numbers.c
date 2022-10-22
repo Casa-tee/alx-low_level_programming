e910e0803c41ed6f24bc0d2aa5b1b1c27961ff58
gift <treasureluke23@gmail.com>
2022-10-21 21:00:36 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,14 @@
 
 /**
  * print_numbers - prints numbers from 0 to 9
- * Return: 0
+ * Return: Always 0
 */
 void print_numbers(void)
 {
 	int b = 0;
 
 	while (b < 10)
-		_putchar(b++ + '0'0);
+		_putchar(b++ + '0');
 
 	_putchar('\n');
 }
