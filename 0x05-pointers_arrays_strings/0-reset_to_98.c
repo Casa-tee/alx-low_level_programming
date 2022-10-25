bfee87290964ab04f11afe9cd6c0a5b05a65d18e
gift <treasureluke23@gmail.com>
2022-10-24 20:47:39 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value of the points
+ * @n: the character to be printed
+ * Return: n
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
