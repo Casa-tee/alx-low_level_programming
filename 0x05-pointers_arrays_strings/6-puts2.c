ae1e78240c14f27259470150bbb06e45823c9cb9
gift <treasureluke23@gmail.com>
2022-10-25 19:24:24 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints out character of a string from the first
+ * @str: the input
+ */
+void puts2(char *str)
+{
+	int i = o;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
