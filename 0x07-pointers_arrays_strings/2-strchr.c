9323536a596f98ac983ae7e9abc9a9bff63e3d51
gift <treasureluke23@gmail.com>
2022-11-01 21:48:25 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,16 +5,17 @@
  * _strchr - Entry point
  * @s: input
  * @c: input
- * Return: Always 0 on success
+ * Return: if c is found - a pointer to the first occurence
+ * 	if c is not found - NULL
  */
 char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (; s[i] <= '\0'; i++)
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-			return (&s[i]);
+			return (s[i]);
 	}
-	return (0);
+	return ('\0');
 }
