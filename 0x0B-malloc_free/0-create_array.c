acba3eea19c257f92e03436157846cd9da93ac97
gift <treasureluke23@gmail.com>
2022-11-08 17:18:58 -0800

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,8 +14,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	array = malloc(size * sizeof(char));
-
-	if (array = NULL)
+	if (array == NULL)
 	{
 		return (NULL);
 	}
